93bdbf778fdadf221b3419bbd1047d90f919dd5b
Brian Corrie <bdcorrie@gmail.com>
2019-04-05 18:13:41 -0700

    Changes to create the first functioning version.
    Note that that is VERY slow on a large repository if used to create
    v_call/j_call graphs as there is no v_call/j_call combined index,
    or more precisely no ir_vgene_family/ir_jgene_family index.

--- api-hackathon/vj_api.py
+++ api-hackathon/vj_api.py
@@ -5,6 +5,7 @@ import os, ssl
 import sys
 import time
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
@@ -63,7 +64,7 @@ def getSamples(sample_url, header_dict, query_dict={}):
         return json.loads('[]')
 
     # Convert the response to JSON so we can process it easily.
-    print(url_response)
+    #print(url_response)
     json_data = json.loads(url_response)
     # Return the JSON data
     return json_data
@@ -81,25 +82,6 @@ def initHTTP():
         getattr(ssl, '_create_unverified_context', None)): 
         ssl._create_default_https_context = ssl._create_unverified_context
 
-# Set the base URL to use 
-#base_url = 'https://ipa1.ireceptor.org'
-#base_url = 'https://ipa2.ireceptor.org'
-#base_url = 'https://ipa3.ireceptor.org'
-#base_url = 'https://ipa4.ireceptor.org'
-#base_url = 'https://vdjserver.org/ireceptor'
-#base_url = 'http://turnkey-test2.ireceptor.org'
-
-# Junction AA length queries.
-#query_key = 'junction_aa_length'
-#query_values = range(40)
-
-# Possible v_call queries
-#query_key = 'v_call'
-#query_values = ['IGHV1','IGHV2','IGHV3','IGHV4','IGHV5','IGHV6','IGHV7']
-#query_key = 'd_call'
-#query_values = ['IGHD1','IGHD2','IGHD3','IGHD4','IGHD5','IGHD6','IGHD7']
-#query_values = ['TRBV1','TRBV2','TRBV3','TRBV4','TRBV5','TRBV6','TRBV7','TRBV8']
-
 def performQueryAnalysis(base_url, query_xkey, query_ykey, query_xvalues, query_yvalues):
     # Ensure our HTTP set up has been done.
     initHTTP()
@@ -110,63 +92,60 @@ def performQueryAnalysis(base_url, query_xkey, query_ykey, query_xvalues, query_
     sample_url = base_url+'/v2/samples'
     sequence_url = base_url+'/v2/sequences_summary'
 
-    sample_json = getSamples(sample_url, header_dict)
-    sample_dict = dict()
-    for sample in sample_json:
-        sample_dict[str(sample['_id'])] = dict()
-
+    # Create a numpy array (zeroed) of the correct size.
+    data = np.zeros((len(query_yvalues), len(query_xvalues)))
     # Iterate over the query values of interest. One query per value gives us results
     # for all samples so this is about as efficient as it gets.
+    # For each of the X query values
+    xindex = 0
     for xvalue in query_xvalues:
+        yindex = 0
+        # For each of the Y query values
         for yvalue in query_yvalues:
+            # Create the query dictinoary values
             query_dict = dict()
-            query_dict.update({query_xkey: xvalue}, {query_ykey:yvalue})
+            query_dict.update({query_xkey: xvalue, query_ykey:yvalue})
             sequence_summary_json = getSequenceSummary(sequence_url, header_dict, query_dict)
-            result = {}
+            # Because we want the total count for all samples for each X,Y pair, we 
+            # iterate over the samples and sum the count for each sample.
             for sample in sequence_summary_json:
-                # Get the dictionaries of values for this sample
-                value_dict = sample_dict[str(sample['_id'])]
-                # Update the count for the value we are considering
-                #filtered_sequence_count = sample['ir_filtered_sequence_count']
-                #value_dict.update({value:filtered_sequence_count})
-                #sample_dict[str(sample['_id'])] = value_dict
-                print('   ' + query_xkey + ' ' + str(xvalue) + 
-                              query_ykey + ',' + str(yvalue) +
-                      ' = ' + str(sample['ir_filtered_sequence_count']))
-
-    data = dict()
-    return data
-    grand_total = 0
-    for sample in sample_json:
-        value_dict = sample_dict[str(sample['_id'])] 
-        sequence_count = sample['ir_sequence_count']
-        print('\nsample = ' + sample['sample_id'] + ' (' + str(sequence_count) + ')')
-        total = 0
-        for key, value in value_dict.items():
-            if key in data:
-                data.update({key:data[key]+value})
-            else:
-                data.update({key:value})
-            print(str(key) + ' = ' + str(value) + ' (' + '%.2f' % ((value/sequence_count)*100.0) + '%)')
-            total = total + value
-            grand_total = grand_total + value
-        print('sample = ' + sample['sample_id'] + ' (' + str(total) + ')')
-        
-    for key, value in data.items():
-        print(str(key) + ' = ' + str(value))
-    print('grand total = ' + str(grand_total))
+                # Update the count for the sample we are considering
+                data[yindex, xindex] = data[yindex, xindex] + sample['ir_filtered_sequence_count']
+                print('   ' + query_xkey + '/' + str(xvalue) +
+                      ' , ' + query_ykey + '/' + str(yvalue) +
+                      ' ( ' + str(xindex) + ',' + str(yindex) + ' ) ' +
+                      ' = ' + str(sample['ir_filtered_sequence_count']) +
+                      ' , ' + str(data[yindex, xindex]))
+            yindex = yindex + 1
+        xindex = xindex + 1
 
     return data
 
-def plotData(plot_names, plot_data, title, filename):
-    #plot_data = list(data.values())
-    #plot_names = list(data.keys())
+def plotData(plot_data, xlabels, ylabels, title, filename):
+    # Plot code borrowed from here: https://matplotlib.org/gallery/images_contours_and_fields/image_annotated_heatmap.html
 
-    plt.rcParams.update({'figure.autolayout': True})
+    matplotlib.use('Agg')
     fig, ax = plt.subplots()
-    ax.barh(plot_names, plot_data)
+    im = ax.imshow(plot_data)
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax)
+    cbar.ax.set_ylabel("Number of Annotations", rotation=-90, va="bottom")
 
-    fig.savefig(filename, transparent=False, dpi=80, bbox_inches="tight")
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(len(xlabels)))
+    ax.set_yticks(np.arange(len(ylabels)))
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+    # ... and label them with the respective list entries
+    ax.set_xticklabels(xlabels)
+    ax.set_yticklabels(ylabels)
+    ax.set_title(title)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+                     rotation_mode="anchor")
+
+    fig.savefig(filename, transparent=False, dpi=240, bbox_inches="tight")
 
 def getArguments():
     parser = argparse.ArgumentParser(
@@ -206,13 +185,11 @@ if __name__ == "__main__":
     xvalues = options.graph_xvalues.split(',')
     yvalues = options.graph_yvalues.split(',')
     # Perform the query analysis, gives us back a dictionary.
-    data = performQueryAnalysis(options.base_url, options.api_xfield, api_yfield, xvalues, yvalues)
-    sys.exit(1)
-    sorted_data = OrderedDict(sorted(data.items(), key=lambda t: t[0]))
+    data = performQueryAnalysis(options.base_url, options.api_xfield, options.api_yfield, xvalues, yvalues)
     # Graph the results
-    title = options.api_field
-    filename = options.api_field + ".png"
-    plotData(list(sorted_data.keys()), list(sorted_data.values()), title, filename)
+    title = options.api_xfield + " " + options.api_yfield + " Usage"
+    filename = options.api_xfield + "_" + options.api_yfield + ".png"
+    plotData(data, xvalues, yvalues, title, filename)
 
     # Return success
     sys.exit(0)
