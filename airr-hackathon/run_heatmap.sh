a55489a4cf83eec96924b00fc310e77eac15305c
Brian Corrie <bdcorrie@gmail.com>
2019-04-13 23:07:52 +0000

    Finished implementing general column heat map implementation

--- airr-hackathon/run_heatmap.sh
+++ airr-hackathon/run_heatmap.sh
@@ -27,18 +27,20 @@ y_column=`cat $filename | awk -F"\t" -v label=$y_column_str '{for(i=1;i<=NF;i++)
 # As a result we chop things off at the first star. This also takes care of the case where
 # a gened call has multiple calls. Since we drop everthing after the first allele we drop all of 
 # the other calls as well.
-cat $filename | cut -f $x_column,$y_column | awk 'BEGIN {FS="\t"; print("v_call\tj_call")} /IG|TR/ {printf("%s\t%s\n",substr($1,0,index($1,"*")-1), substr($2,0,index($2,"*")-1))}' > $filename2
+cat $filename | cut -f $x_column,$y_column | awk -v xlabel=$x_column_str -v ylabel=$y_column_str 'BEGIN {FS="\t"; printf("%s\t%s\n", xlabel, ylabel)} /IG|TR/ {printf("%s\t%s\n",substr($1,0,index($1,"*")-1), substr($2,0,index($2,"*")-1))}' > $filename2
 
 # Generate a set of unique values that we can generate the heatmap on. This is a comma separated
 # list of unique gene names for each of the two fields of interest.
 xvals=`cat $filename2 | cut -f 1 | awk 'BEGIN {FS=","} /IG/ {print($1)} /TR/ {print($1)}' | sort | uniq | awk '{if (NR>1) printf(",%s", $1); else printf("%s", $1)}'`
-echo "$xvals"
 yvals=`cat $filename2 | cut -f 2 | awk 'BEGIN {FS=","} /IG/ {print($1)} /TR/ {print($1)}' | sort | uniq | awk '{if (NR>1) printf(",%s", $1); else printf("%s", $1)}'`
-echo "$yvals"
 
 # Finally we generate a heatmap given all of the processed information.
-python3 airr_heatmap.py v_call j_call $xvals $yvals $filename2 $x_column_str-$y_column_str-sample$sample.png
+echo "$x_column_str"
+echo "$y_column_str"
+echo "$xvals"
+echo "$yvals"
+python3 airr_heatmap.py $x_column_str $y_column_str $xvals $yvals $filename2 $x_column_str-$y_column_str-sample$sample.png
 
 # Clean up our temporary files.
-rm $filename
-rm $filename2
+#rm $filename
+#rm $filename2
