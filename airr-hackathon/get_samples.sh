36587f50e118c0ce4247a4cf034f68e2f64210a4
Brian Corrie <bdcorrie@gmail.com>
2019-04-13 23:06:54 +0000

    Added checking for command line parameters.

--- airr-hackathon/get_samples.sh
+++ airr-hackathon/get_samples.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+if [ $# -ne 1 ];
+then
+    echo "Usage: $0 BaseURL"
+    exit 1
+fi
+
 curl -s -S -k -X POST  -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" $1/v2/samples
 
