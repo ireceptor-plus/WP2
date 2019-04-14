11e9a6c2ef6724a640fa46e21d2f2b16bb2396da
Brian Corrie <bdcorrie@gmail.com>
2019-04-14 00:02:50 +0000

    Simple shell script that takes a URL, a sample ID, and an AIRR field name and generates a histogram for that field.

---
+++ airr-hackathon/run_histogram.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ne 3 ];
+then
+    echo "Usage: $0 sampleID fieldName baseURL"
+    exit 1
+fi
+
+sample=$1
+field_name=$2
+url=$3
+
+filename=/tmp/ipa1_sample$sample.tsv
+
+# Get the data from the repository at the URL provided.
+curl -k -X POST  -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "ir_project_sample_id_list[]=$sample" -d "output=tsv" -d "ir_data_format=airr" $url/v2/sequences_data > $filename
+
+# Finally we generate a heatmap given all of the processed information.
+python3 airr_histogram.py $field_name $filename $field_name-sample$sample.png
+
+# Clean up our temporary files.
+#rm $filename
