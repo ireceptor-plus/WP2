447637f5faccdceaff68868f85ba4f9b87adc2f4
Brian Corrie <bdcorrie@gmail.com>
2019-08-16 22:13:03 +0000

    Made fixes so searching for a specific sample works...

--- api-hackathon/graph_sample_api.py
+++ api-hackathon/graph_sample_api.py
@@ -23,8 +23,10 @@ def getSequenceSummary(sequence_url, header_dict, query_dict={}):
         response = urllib.request.urlopen(request)
         url_response = response.read().decode(response.headers.get_content_charset())
     except urllib.error.HTTPError as e:
-        print('Error: Server could not fullfil the request')
+        print('Error: Server could not fullfil the request: ', sequence_url)
         print('Error: Error code =', e.code)
+        print('Error: Request header = ', header_dict)
+        print('Error: Request query = ', query_dict)
         print(e.read())
         return json.loads('[]')
     except urllib.error.URLError as e:
@@ -48,7 +50,7 @@ def getSamples(sample_url, header_dict, query_dict={}):
     #url_dict.update(header_dict)
     url_dict.update(query_dict)
     url_data = urllib.parse.urlencode(url_dict).encode()
-    print(url_data)
+    #print(url_data)
 
     # Try to connect the URL and get a response. On error return an
     # empty JSON array.
@@ -58,8 +60,10 @@ def getSamples(sample_url, header_dict, query_dict={}):
         response = urllib.request.urlopen(request)
         url_response = response.read().decode(response.headers.get_content_charset())
     except urllib.error.HTTPError as e:
-        print('Error: Server could not fullfil the request')
+        print('Error: Server could not fullfil the request: ', sample_url)
         print('Error: Error code =', e.code)
+        print('Error: Request header = ', header_dict)
+        print('Error: Request query = ', query_dict)
         print(e.read())
         return json.loads('[]')
     except urllib.error.URLError as e:
@@ -101,17 +105,28 @@ def performQueryAnalysis(base_url, sample_id, query_key, query_values):
     # Create a dictionary with keys as the ID for the sample.
     sample_dict = dict()
     # For each sample, create an empty dictionary (to be filled in later)
+    sample_count = 0
     for sample in sample_json:
-        #sample_dict[str(sample['_id'])] = dict()
         sample_dict[str(sample['ir_project_sample_id'])] = dict()
+        ir_project_sample_id_str = str(sample['ir_project_sample_id'])
+        sample_count = sample_count + 1
+
+    # Check to see that we found one and only one result.
+    if sample_count == 0:
+        print("Error: Could not find sample_id ", sample_id)
+        return dict()
+    if sample_count > 1:
+        print("Error: Expecting only one sample returned from query, found " + str(sample_count))
+        return dict()
+    print("Found sample_id " + sample_id + ", id = " + ir_project_sample_id_str)
 
     # Iterate over the query values of interest. One query per value gives us results
     # for all samples so this is about as efficient as it gets.
     for value in query_values:
         # Create and do the query for this value.
         query_dict = dict()
-        query_dict.update({query_key: value})
-        print('Performing query: ' + str(query_key) + ' = ' + str(value))
+        query_dict.update({'ir_project_sample_id_list[]': ir_project_sample_id_str, query_key: value})
+        #print('Performing query: ', query_dict)
         sequence_summary_json = getSequenceSummary(sequence_url, header_dict, query_dict)
         #print(sequence_summary_json)
         # The query gives us a count for each sample. We iterate over the samples to do some
@@ -216,6 +231,9 @@ if __name__ == "__main__":
     options = getArguments()
     # Split the comma separated input string.
     values = options.graph_values.split(',')
+    #print(options.graph_values)
+    #print(values)
+
     # Perform the query analysis, gives us back a dictionary.
     t_start = time.perf_counter()
     data = performQueryAnalysis(options.base_url, options.sample_id, options.api_field, values)
@@ -223,9 +241,9 @@ if __name__ == "__main__":
     print("Total query time =", (t_end - t_start), "seconds", flush=True)
     sorted_data = OrderedDict(sorted(data.items(), key=lambda t: t[0]))
     # Graph the results
-    title = options.base_url + " - " + options.api_field
+    title = options.base_url + " - " + options.sample_id + " - " + options.api_field
     url_info = urllib.parse.urlparse(options.base_url)
-    filename = url_info.netloc + "-" + options.api_field + ".png"
+    filename = url_info.netloc + "-" + options.sample_id + "-" + options.api_field + ".png"
     plotData(list(sorted_data.keys()), list(sorted_data.values()), title, filename)
 
     # Return success
