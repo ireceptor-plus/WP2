27fb05a8e2d9c9161676625055c70b68d3558944
Brian Corrie <bdcorrie@gmail.com>
2019-08-22 22:31:53 +0000

    Changed from using curl to more extensive python checks.

--- api-hackathon/adc-api/ADCTest/ADCtest.sh
+++ api-hackathon/adc-api/ADCTest/ADCtest.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Get the location of the directory where the script was run.
+PROG_NAME=`basename "$0"`
 SCRIPT_DIR=`dirname "$0"`
+PYTHON_PROG="test_airr_api.py"
+
+# Environment checks
+#echo $PROG_NAME
+#echo $SCRIPT_DIR
+#python --version
+
 
 NB_ARGS=3
 if [ $# -lt $NB_ARGS ];
 then
-    echo "$0: wrong number of arguments ($# instead of at least $NB_ARGS)"
-    echo "usage: $0 adc_url entry_point <json_query> [<json_query> ...]"
+    echo "$PROG_NAME: wrong number of arguments ($# instead of at least $NB_ARGS)"
+    echo "usage: $PROG_NAME adc_url entry_point <json_query> [<json_query> ...]"
     exit 1
 fi
 
@@ -19,20 +27,25 @@ entry_point="$1"
 shift
 
 # For the remainder of command line parameters, treat each as a JSON query.
+let "error_count=0"
+let "total_count=0"
 while [ "$1" != "" ]; do
     filename="$1"
-    # Do the CURL command and report if the command exits properly
-    # The --fail tells curl to fail with error 22 on an HTTP error.
-    echo "curl -s --data @$filename https://$adc_url/$entry_point"
-    curl -s --fail --data @$filename https://$adc_url/$entry_point 
+    # Run the python code (python 3 required) to test the API query
+    python3 $SCRIPT_DIR/$PYTHON_PROG $adc_url $entry_point $filename
     error_code=$?
     if [ $error_code -ne 0 ]
     then
-        echo "ERROR: Query $filename to $adc_url/$entry_point failed with exit code: $?"
-    else
-        echo "Query $filename to $adc_url/$entry_point passed!"
+        let "error_count=error_count+1"
     fi
     # Process the next command line arguement.
     shift
+    let "total_count=total_count+1"
 done
 
+if [ $error_count -gt 0 ]
+then
+    echo "SUMMARY: $error_count of $total_count tests failed"
+else
+    echo "SUMMARY: All tests passed!!!"
+fi
